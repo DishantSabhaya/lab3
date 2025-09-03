955c154b6980b924985c835c8cbd1e588898cbce
DishantSabhaya <dishantsabhaya2007@gmail.com>
2025-09-03 12:52:02 +0530

    Add files via upload

---
+++ 1 to 10 even no.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+     int a;
+     printf("the first 10 even numbers are:\n");
+     for(a=2; a <= 10; a+=2) {
+     printf("%d", a);
+     }
+
+
+ }
+
