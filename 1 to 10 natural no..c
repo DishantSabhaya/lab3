1f872f048fb59cb4d8159a6a149330ab4ac7ffcb
DishantSabhaya <dishantsabhaya2007@gmail.com>
2025-09-03 12:32:22 +0530

    Add files via upload

---
+++ 1 to 10 natural no..c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+     int a;
+     printf("the first 10 natural numbers are:\n");
+     for(a=1; a <= 10; a++) {
+     printf("%d", a);
+     }
+
+
+ }
