5212766d4fb7ef363b6cc1b0cc72efebc1324593
DishantSabhaya <dishantsabhaya2007@gmail.com>
2025-09-03 12:45:28 +0530

    Add files via upload

---
+++ 1 to 10 odd no.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+     int a;
+     printf("the first 10 odd numbers are:\n");
+     for(a=1; a <= 19; a+=2) {
+     printf("%d", a);
+     }
+
+
+ }
+
+
